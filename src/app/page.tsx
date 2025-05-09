3155822c7807ef6d6df395344816b3de9348e2f9
Nelwin Catalogo <catalogonelwin@gmail.com>
2025-05-09 18:36:52 +0800

    ✨ feat(template): add various components and utilities
    - Add loading components and icon
    - Introduce new sample components (Dialogs, Alerts, React Query)
    - Implement custom UI components (Date Picker, Badge, Dialog, Input, Sonner)
    - Update global styles with utilities and loading animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import CounterSample from '@/components/samples/counter.sample';
+import Template from '@/components/samples/template';
 
 export default function Home() {
 	return (
-		<div className="p-8">
-			<h1 className="text-center text-4xl font-bold text-red-500">
-				Next-Tailwind Starter Template
-			</h1>
-
-			<div className="mt-8 grid place-items-center bg-gray-200 p-8">
-				<CounterSample />
-			</div>
+		<div>
+			<Template />
 		</div>
 	);
 }
--- src/components/ui-customs/sonner.tsx
+++ src/components/ui-customs/sonner.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { Toaster as Sonner, ToasterProps } from 'sonner';
+import { Toaster as Sonner } from 'sonner';
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-	const { theme = 'system' } = useTheme();
+	const { theme = 'light' } = useTheme();
 
 	return (
 		<Sonner
@@ -14,7 +16,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
 				classNames: {
 					toast:
 						'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-					description: 'group-[.toast]:!text-muted-foreground',
+					description: 'group-[.toast]:text-muted-foreground',
 					actionButton:
 						'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
 					cancelButton:
@@ -22,13 +24,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
 					icon: 'group-data-[type=error]:text-red-500 group-data-[type=success]:text-green-500 group-data-[type=warning]:text-amber-500 group-data-[type=info]:text-blue-500',
 				},
 			}}
-			style={
-				{
-					'--normal-bg': 'var(--popover)',
-					'--normal-text': 'var(--popover-foreground)',
-					'--normal-border': 'var(--border)',
-				} as React.CSSProperties
-			}
 			{...props}
 		/>
 	);
